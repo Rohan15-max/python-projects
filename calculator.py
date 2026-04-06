71a6f3a198c489c0bb85de6e7262e78629de91ce
Rohan15-max <rohangaud2007@gmail.com>
2026-04-06 20:20:51 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("Simple Calculator by Rohan")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+op = input("Enter operation (+, -, *, /): ")
+
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operation")
